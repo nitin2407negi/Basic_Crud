d126f255bbd817eacb1991fcd3bee2dd1362240f
nitin2407negi <nitinnegi241@gmail.com>
2026-05-05 11:15:06 +0530

    fix: added TypeOrmModule forFeature in movie module

--- src/modules/movie/movie.module.ts
+++ src/modules/movie/movie.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MovieController } from './movie.controller';
+import { Movie } from './entities/movie.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MovieService } from './movie.service';
 
 @Module({
+   imports: [
+    TypeOrmModule.forFeature([Movie]),
+  ],
   controllers: [MovieController],
   providers: [MovieService]
 })
